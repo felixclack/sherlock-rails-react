575531a966359d2d9cfdf36b290e655688831974
Felix Clack <felixclack@gmail.com>
2019-04-05 21:24:03 +1000

    Set the root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  resources :pages
   post "/graphql", to: "graphql#execute"
-  match '*path', to: 'pages#index', via: :all
+  match '/*path', to: 'pages#index', via: :all
+  root 'pages#index'
 end
