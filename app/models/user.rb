9945bb987546f1efca9b6ca00afe6596948e21cd
Felix Clack <felixclack@gmail.com>
2019-04-05 14:51:22 +1000

    Ensure user data is valid
    The GraphQL schema defines that all the user attributes are not null but
    currently our data model allows this case to happen.
    
    We can use validations to ensure that the values are present.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :birthdate, presence: true
+  validates :job, presence: true
+  validates :name, presence: true
+  validates :phone, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_properties = {
+      name: 'Test',
+      birthdate: '1974-01-01',
+      phone: '123-4567-8901',
+      job: 'Tester'
+    }
+  end
+
+  test 'requires a name' do
+    user = User.new(@user_properties.except(:name))
+    refute user.valid?
+    assert_not_nil user.errors[:name]
+  end
+
+  test 'requires a birthdate' do
+    user = User.new(@user_properties.except(:birthdate))
+    refute user.valid?
+    assert_not_nil user.errors[:birthdate]
+  end
+
+  test 'requires a phone' do
+    user = User.new(@user_properties.except(:phone))
+    refute user.valid?
+    assert_not_nil user.errors[:phone]
+  end
+
+  test 'requires a job' do
+    user = User.new(@user_properties.except(:job))
+    refute user.valid?
+    assert_not_nil user.errors[:job]
+  end
 end
